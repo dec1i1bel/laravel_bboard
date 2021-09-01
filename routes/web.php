0f065940d0e4d19f889358b2bcb118185bcedcaa
Vasiliy <v.balabanov85@gmail.com>
2021-09-01 06:34:02 +0300

    framework comments removed

--- routes/web.php
+++ routes/web.php
@@ -4,35 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BbsController;
 use App\Http\Controllers\HomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/create', function() {
-//     return view('/create');
-// });
-
 Route::get('/', [BbsController::class, 'index'])->name('index');
-// Route::post('/store' , [BbsController::class, 'store']);
 
-/**
- * метод создаёт маршруты на действия контроллеров, созданных через php artisan ui:auth
- * также здесь создаются настройки безопасности
- */
 Auth::routes();
 
-/**
- * /home - раздела пользователя
- * 
- * name('home') - имя маршрута
- */
 Route::get('/home', [HomeController::class, 'index'])
     ->name('home');
 
